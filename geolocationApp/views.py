97676b4c7a5759513229a3c0b6cb2edeb724d95c
maikel <maikel@laboratorioPrakticas.laboratorioPraktikas>
2021-10-04 15:06:17 +0200

    Obtaining coordinates and printing on the map

--- geolocationApp/views.py
+++ geolocationApp/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+import requests
+import json
+import folium
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    
+    ip = requests.get('https://api.ipify.org?format=json')
+    ip_data = json.loads(ip.text)
+    res = requests.get('http://ip-api.com/json/'+ip_data['ip'])
+    location_data = json.loads(res.text)
+
+    point = (location_data['lat'],location_data['lon'])
+    folium_map= folium.Map(width=800,height=500,location=point)
+    folium.Marker(
+                  [location_data['lat'],location_data['lon']],
+                  tooltip='click here for more information',
+                  popup=location_data['city'],
+                  icon=folium.Icon(color='red')
+                  ).add_to(folium_map)
+    folium_map = folium_map._repr_html_()
+
+    context = {
+        'data':location_data,
+        'map':folium_map
+    }
+
+    return render(request,'index.html',context)
